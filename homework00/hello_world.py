afd886c42c393b4aa63d0fee757e5cc4428e1467
Iwtdssss <grishgord@gmail.com>
2026-09-21 11:33:08 +0300

    Correct text function definition and docstring
    Fix function definition and docstring formatting.

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,12 @@
-def text():
+""" Лаба 0 """
+
+def text(message: str="default") -> str":
+    """Function
+
+    param message: message
+    return: message
+    """
     return message
+
+if __name__ == "__main__":
+    print(text())
